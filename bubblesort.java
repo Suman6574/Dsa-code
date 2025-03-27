96fb95e148a8608b9655476c990b0bef42006a51
Suman Kumar <krsumansingh2002@gmail.com>
2025-03-27 11:09:04 +0530

    suman changes

--- bubblesort.java
+++ bubblesort.java
@@ -22,7 +22,7 @@ public class bubblesort {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 6, 0, 5, 3, 4, 1, 2 };
+        int arr[] = { 6, 5, 3, 4, 1, 2 };
         sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
